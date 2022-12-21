7d79aab07afdb0c1b0923e0397309414b4fc0885
JumaboyevQuvondiq <jumaboyevquvondiqm5202@gmail.com>
2022-12-22 01:54:17 +0500

    Create Ef core and migration

---
+++ src/TwistFood.Api/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace TwistFood.Api.Models;
+
+public class Product
+{
+    public long Id { get; set; }
+    public int CategoryId { get; set; } 
+    public virtual Category Category { get; set; }
+    public string ProductName { get; set; } = string.Empty; 
+    public string ProductDescription { get; set; }= string.Empty;
+    public string ImagePath { get; set; } = string.Empty;
+    public double Price { get; set; }   
+
+}
