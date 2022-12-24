e5b48b71a2373315476b0cf3779c80b6c3eaf3f9
JumaboyevQuvondiq <jumaboyevquvondiqm5202@gmail.com>
2022-12-24 18:18:54 +0500

    Create user and admin repository. create PaginationService

---
+++ src/TwistFood.DataAccess/Interfaces/IAdminService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwistFood.Api.Models;
+using TwistFood.DataAccess.Common.Utils;
+
+namespace TwistFood.DataAccess.Interfaces;
+
+public interface IAdminService
+{
+    public Task<IEnumerable<Admin>> GetAllAsync(PagenationParams @params);
+    public Task<Admin> GetAsync(int id);
+    public Task<bool> CreateAsync(Admin admin);
+    public Task<bool> DeleteAsync(int id);
+    public Task<bool> UpdateAsync(int id, Admin admin);
+}
---
+++ src/TwistFood.Domain/Exceptions/StatusCodeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwistFood.Domain.Exceptions;
+
+public class StatusCodeException : Exception
+{
+    public HttpStatusCode StatusCode { get; set; }
+
+    public StatusCodeException(HttpStatusCode statusCode, string message)
+        : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
