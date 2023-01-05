9606a484c15606d5e4f77e3a478da0df645e0ddd
A'zamjon Shaydullayev <azamjondev@gmail.com>
2023-01-05 16:43:34 +0500

    updated

---
+++ src/TwistFood.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CarShop.Api.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
