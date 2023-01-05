dcf4d625ed8a08ed499e38a8665805c90c68b2a3
A'zamjon Shaydullayev <azamjondev@gmail.com>
2023-01-05 16:33:35 +0500

    add exceptionhandler middleware

---
+++ src/TwistFood.Service/Common/Exceptions/StatusCodeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwistFood.Service.Common.Exceptions
+{
+    public class StatusCodeException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+
+        public StatusCodeException(HttpStatusCode statusCode, string message)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
