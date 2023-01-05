a16a3ebce022650e2fb3e3a1e7a4a1a3ecc5cf35
A'zamjon Shaydullayev <azamjondev@gmail.com>
2023-01-05 16:41:36 +0500

    updated

---
+++ src/TwistFood.Service/Exceptions/StatusCodeException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace CarShop.Api.Common.Exceptions;
+public class StatusCodeException : Exception
+{
+    public HttpStatusCode StatusCode { get; set; }
+
+    public StatusCodeException(HttpStatusCode statusCode, string message)
+        : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
